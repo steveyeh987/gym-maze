8c362ec2e6fe839ef0efb481962d1ed1547da417
changlintsai <42146530+changlintsai@users.noreply.github.com>
2018-08-08 20:27:35 +0800

    Update __init__.py

--- gym_maze/envs/__init__.py
+++ gym_maze/envs/__init__.py
@@ -1,2 +1,2 @@
-from gym_maze.envs.maze_env import mazeEnv
-from gym_maze.envs.maze_extrahard_env import mazeExtraHardEnv
+from gym_maze.envs.maze_env import MazeEnv
+from gym_maze.envs.maze_extrahard_env import MazeExtraHardEnv
