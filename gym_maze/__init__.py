0c33b5536679f097208d0c7e2aa74dd93a406f44
changlintsai <42146530+changlintsai@users.noreply.github.com>
2018-08-08 20:26:41 +0800

    Update __init__.py

--- gym_maze/__init__.py
+++ gym_maze/__init__.py
@@ -2,9 +2,9 @@ from gym.envs.registration import register
 
 register(
     id='maze-v0',
-    entry_point='gym_maze.envs:mazeEnv',
+    entry_point='gym_maze.envs:MazeEnv',
 )
 register(
     id='maze-extrahard-v0',
-    entry_point='gym_maze.envs:mazeExtraHardEnv',
+    entry_point='gym_maze.envs:MazeExtraHardEnv',
 )
